0a027cf2e23edcf025d93d4fdafbf6a7c90a7954
abiola boluwatife <abiol5202@gmail.com>
2018-12-27 08:46:56 +0100

    removed jquery and popper

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import $ from 'jquery';
-import Popper from 'popper.js';
 import ReactDOM from 'react-dom';
 import './Assets/vendor/fontAwesomepro/css/all.css'
 import './index.css';
